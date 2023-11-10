6ed544bf979e9aff8565d06caa29523241627d6a
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-10 14:37:40 -0600

    :tada: Begin a proyect

---
+++ src/utils/jsonCertificados.js
@@ -0,0 +1,34 @@
+export default jsonCertificados = [
+  {
+    id: 1,
+    title: "",
+    img: "",
+    description: "",
+  },
+  {
+    id: 2,
+    title: "",
+    img: "",
+    description: "",
+  },
+  {
+    id: 3,
+    title: "",
+    img: "",
+    description: "",
+  },
+  {
+    id: 4,
+    title: "",
+    img: "",
+    description: "",
+  },
+  {
+    id: 5,
+    title: "",
+    img: "",
+    description: "",
+  },
+];
+
+console.log(jsonCertificados);
